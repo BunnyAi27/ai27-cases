44038c9ecacb4e6413f2bfcf9d5db11a06b71629
BunnyAi27 <135638399+BunnyAi27@users.noreply.github.com>
2023-10-23 11:08:31 +0200

    Added list Case and Template functionality

--- src/helpers/connectCasesHelper.js
+++ src/helpers/connectCasesHelper.js
@@ -1,4 +1,4 @@
-const { ConnectCasesClient, CreateCaseCommand, GetCaseCommand, UpdateCaseCommand } = require("@aws-sdk/client-connectcases")
+const { ConnectCasesClient, CreateCaseCommand, GetCaseCommand, UpdateCaseCommand, ListCasesForContactCommand, ListTemplatesCommand } = require("@aws-sdk/client-connectcases")
 const client = new ConnectCasesClient();
 const parameterBuilder = require("./connectParameterBuilder");
 const responseBuilder = require("./responseBuilder");
@@ -51,6 +51,41 @@ const connectCaseAction = async (event, tenantConfig) => {
           return responseBuilder.formatResponse(event, 200, response);
         } catch (error) {
           console.error("Get response: ", error);
+          return responseBuilder.formatResponse(event, 400, error);
+        }
+      case "/listCases":
+        try {
+          const listCaseParams = parameterBuilder.listCaseParameterBuilder(eventBody, tenantConfig);
+          console.info("List Case Params: ", JSON.stringify(listCaseParams));
+          command = new ListCasesForContactCommand(listCaseParams);
+          response = await client.send(command);
+          console.info("List Case response: ", response);
+
+          while (response?.nextToken && response.nextToken !== null){
+            let nextToken = response.nextToken;
+            listCaseParams = parameterBuilder.getParameterBuilder(eventBody, tenantConfig, nextToken);
+            console.info("List Case Params: ", JSON.stringify(listCaseParams));
+            listCaseParams['nextToken'] = response.nextToken;
+            command = new ListCasesForContactCommand(listCaseParams);
+            let nextResponse = await client.send(command);
+            response = response.cases.concat(nextResponse.cases);
+          }
+
+          return responseBuilder.formatResponse(event, 200, response);
+        } catch (error) {
+          console.error("List Case response: ", error);
+          return responseBuilder.formatResponse(event, 400, error);
+        }
+      case "/listTemplate":
+        try {
+          const listTemplateParams = parameterBuilder.listTemplateParameterBuilder(eventBody, tenantConfig);
+          console.info("List Template Params: ", JSON.stringify(listTemplateParams));
+          command = new ListTemplatesCommand(listTemplateParams);
+          response = await client.send(command);
+          console.info("List Template response: ", response);
+          return responseBuilder.formatResponse(event, 200, response);
+        } catch (error) {
+          console.error("List Template response: ", error);
           return responseBuilder.formatResponse(event, 400, error);
         }
 
